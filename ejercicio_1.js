80ceae471812246ea815955f37541736f9ef374a
Alexa276 <AlexaReyesRojas02@gmail.com>
2021-12-14 09:31:36 -0400

    ejercicio_1

---
+++ ejercicio_1.js
@@ -0,0 +1,10 @@
+/*Escriba un programa al cual se le digite un número e imprima por consola su nombre  cuantas veces como lo indique el número digitado. 
+ */
+
+var num = 5;
+var name = "Alexa";
+
+for (let i=0; i< num; ++i){
+
+    console.log (name);
+}
