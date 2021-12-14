472b3b04ca0c3191685c65b66ec9891c8c9a3d19
Alexa276 <AlexaReyesRojas02@gmail.com>
2021-12-14 11:07:50 -0400

    ejercicio_2

---
+++ ejercicio_2.js
@@ -0,0 +1,15 @@
+/*Haga un programa al cual se le digite un número entero y despliegue por pantalla la suma de  los números 
+comprendidos entre 0 y el número digitado */
+
+let num = 20;
+let sum = 0;
+
+// not!
+
+while (! (num ==0) ){
+
+    sum += num;
+    --num
+}
+
+console.log(sum);
