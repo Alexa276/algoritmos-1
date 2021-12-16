3b771d580439beac31cba1db42c9ff87dbb50b54
Alexa276 <AlexaReyesRojas02@gmail.com>
2021-12-16 18:54:38 -0400

    ejercicio_23

---
+++ ejercicio_23.js
@@ -0,0 +1,22 @@
+/*Diseñar el algoritmo que nos permite calcular los n primeros términos de la serie Fibonacci 
+(donde n es ingresado por el usuario).
+ */
+
+function fibonacci(n) {
+    var a = 0;
+    var b = 1;
+    var c = 0;
+    var i = 0;
+    var resultado = "";
+    while (i < n) {
+        c = a + b;
+        a = b;
+        b = c;
+        resultado += c + " ";
+        i++;
+    }
+    return resultado;
+}
+
+var n = parseInt(prompt("Ingrese el número de términos de la serie Fibonacci que desea mostrar: "));
+alert(fibonacci(n));
