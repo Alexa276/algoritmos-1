a1c7c1345ec314e48195a5eed8d510b23c030409
Alexa276 <AlexaReyesRojas02@gmail.com>
2021-12-16 19:00:54 -0400

    ejercicio_11

--- ejercicio_11.js
+++ ejercicio_11.js
@@ -4,56 +4,23 @@ natalidad y de mortalidad esperadas. Las tasas se expresan  como porcentajes de 
 tasa de natalidad es de un 1.5 %  entonces el número de personas nacidas el año n se espera que sea el 1.5 % de 
 la población del año n-1.  */
 
-let poblacionActual = prompt(`Introduzca la poblacion actual`)*1;
-let tasaNatalidad = prompt(`Introduzca la tasa de natalidad`)*1;
-let tasaMortalidad = prompt(`Introduzca la tasa de mortalidad`)*1;
-var natalidad = 0;
-var mortalidad = 0;
 
-// operaciones 
+var poblacion_actual = 1 * prompt ("Introduce cantidad personas que hay en la poblacion actualmente.");
 
-for (let i = 0; i <= 10; i++) {
-    natalidad = (tasaNatalidad/100)*poblacionActual;
-    mortalidad = (tasaMortalidad/100)*poblacionActual;
-    poblacionActual += (natalidad - mortalidad);  
+var tasa_natalidad = 1 * prompt ("Introduce tasa de natalidad de la poblacion.") / 100;
+
+var tasa_mortalidad = 1 * prompt ("Introduce tasa de mortalidad de la poblacion.") / 100;
+
+var natalidad;
+
+var mortalidad;
+
+
+for ( i = 1; i <= 10; i++) {
+    natalidad = tasa_natalidad * poblacion_actual;
+    mortalidad = tasa_mortalidad * poblacion_actual;
+    poblacion_actual += (natalidad - mortalidad);    
     document.getElementById ("tabla").rows [i].cells [1].innerText = Math.round(natalidad);
     document.getElementById ("tabla").rows [i].cells [2].innerText = Math.round(mortalidad);
-    document.getElementById ("tabla").rows [i].cells [3].innerText = Math.round(poblacionActual);
-    
-}
-
-
-function genera_tabla() {
-    // Obtener la referencia del elemento body
-    var body = document.getElementsByTagName("body")[0];
-  
-    // Crea un elemento <table> y un elemento <tbody>
-    var tabla   = document.createElement("table");
-    var tblBody = document.createElement("tbody");
-  
-    // Crea las celdas
-    for (var i = 0; i < 2; i++) {
-      // Crea las hileras de la tabla
-      var hilera = document.createElement("tr");
-  
-      for (var j = 0; j < 2; j++) {
-        // Crea un elemento <td> y un nodo de texto, haz que el nodo de
-        // texto sea el contenido de <td>, ubica el elemento <td> al final
-        // de la hilera de la tabla
-        var celda = document.createElement("td");
-        var textoCelda = document.createTextNode("celda en la hilera "+i+", columna "+j);
-        celda.appendChild(textoCelda);
-        hilera.appendChild(celda);
-      }
-  
-      // agrega la hilera al final de la tabla (al final del elemento tblbody)
-      tblBody.appendChild(hilera);
-    }
-  
-    // posiciona el <tbody> debajo del elemento <table>
-    tabla.appendChild(tblBody);
-    // appends <table> into <body>
-    body.appendChild(tabla);
-    // modifica el atributo "border" de la tabla y lo fija a "2";
-    tabla.setAttribute("border", "2");
-  };
+    document.getElementById ("tabla").rows [i].cells [3].innerText = Math.round(poblacion_actual);
+};
